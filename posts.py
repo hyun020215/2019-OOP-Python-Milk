3e22a04d2d848416d700639280e1b622ef7e0cd6
hyun020215 <hyun020215@sasa.hs.kr>
2019-11-12 20:51:01 +0900

    Post 클래스를 post.py 안에 만들었습니다.

--- posts.py
+++ posts.py
@@ -4,7 +4,7 @@ class Post:
         self.text = text
         self.like = like
         self.comment = comment
-        self.topic = 'default'
+        self.topic = []
 
     def has_keywords(self, *keywords):
         for i in keywords:
@@ -12,5 +12,24 @@ class Post:
                 return True
         return False
 
-    def set_topic(self, topic):
-        self.topic = topic
+    def find_topic(self):
+        if self.has_keywords(['기숙사', '사감', '정독', '소학', '요양', '벌점', '자습', '냉장', '점호', '폰', '스탠딩']):
+            self.topic.append('dormitory')
+        if self.has_keywords(['급식', '영양사', '조리사', '메뉴', '점심', '잔반', '배식']):
+            self.topic.append('cafeteria')
+        if self.has_keywords(['입시', '대학', '면접', '논술', '원서']):
+            self.topic.append('college')
+        if self.has_keywords(['음악실', '노래방', '악기', '피아노', '드럼', '바이올린']):
+            self.topic.append('music')
+        if self.has_keywords(['신입생', '예비', '입학']):
+            self.topic.append('freshmen')
+        if self.has_keywords(['대나무숲', '페이지', '대숲', '건의사항', '신문고', '규정']):
+            self.topic.append('suggestion')
+        if self.has_keywords(['본관', '도서관', '공강', '성적', '미술실', '레이저', '홈룸', '수업', '체육관']):
+            self.topic.append('school')
+        if self.has_keywords(['쓰레기', '청소']):
+            self.topic.append('clean')
+        if self.has_keywords(['종소리', '벨소리', '기상송', '방송부']):
+            self.topic.append('bell')
+        if self.has_keywords(['공', '운동장']):
+            self.topic.append('exercise')
