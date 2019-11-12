5635770bd73fe50f758a4a2b921ce9a230692440
hyun020215 <hyun020215@sasa.hs.kr>
2019-11-12 22:57:49 +0900

    Post class created Graph class created README.md completed

--- posts.py
+++ posts.py
@@ -1,36 +1,38 @@
 class Post:
+    words = {
+        '기숙사': ['기숙사', '사감', '정독', '소학', '요양', '벌점', '자습', '냉장', '점호', '폰', '스탠딩'],
+        '급식': ['급식', '영양사', '조리사', '메뉴', '점심', '잔반', '배식'],
+        '입시': ['입시', '대학', '면접', '논술', '원서'],
+        '음악실': ['음악실', '노래방', '악기', '피아노', '드럼', '바이올린'],
+        '신입생': ['신입생', '예비', '입학'],
+        '건의사항': ['대나무숲', '페이지', '대숲', '건의사항', '신문고', '규정', '폰'],
+        '본관': ['본관', '도서관', '공강', '성적', '미술실', '레이저', '홈룸', '수업', '체육관'],
+        '청결': ['쓰레기', '청소'],
+        '종소리': ['종소리', '벨소리', '기상송', '방송부'],
+        '운동': ['운동장']
+    }
+
     def __init__(self, date, text, like=0, comment=0):
         self.date = date
         self.text = text
         self.like = like
         self.comment = comment
-        self.topic = []
+        self.category = []
 
-    def has_keywords(self, *keywords):
+    def has_keywords(self, keywords):
         for i in keywords:
             if i in self.text:
                 return True
         return False
 
-    def find_topic(self):
-        if not self.topic:
-            if self.has_keywords(['기숙사', '사감', '정독', '소학', '요양', '벌점', '자습', '냉장', '점호', '폰', '스탠딩']):
-                self.topic.append('dormitory')
-            if self.has_keywords(['급식', '영양사', '조리사', '메뉴', '점심', '잔반', '배식']):
-                self.topic.append('cafeteria')
-            if self.has_keywords(['입시', '대학', '면접', '논술', '원서']):
-                self.topic.append('college')
-            if self.has_keywords(['음악실', '노래방', '악기', '피아노', '드럼', '바이올린']):
-                self.topic.append('music')
-            if self.has_keywords(['신입생', '예비', '입학']):
-                self.topic.append('freshmen')
-            if self.has_keywords(['대나무숲', '페이지', '대숲', '건의사항', '신문고', '규정']):
-                self.topic.append('suggestion')
-            if self.has_keywords(['본관', '도서관', '공강', '성적', '미술실', '레이저', '홈룸', '수업', '체육관']):
-                self.topic.append('school')
-            if self.has_keywords(['쓰레기', '청소']):
-                self.topic.append('clean')
-            if self.has_keywords(['종소리', '벨소리', '기상송', '방송부']):
-                self.topic.append('bell')
-            if self.has_keywords(['운동장']):
-                self.topic.append('exercise')
+    def find_category(self):
+        if not self.category:
+            for topic, keywords in Post.words.items():
+                if self.has_keywords(keywords):
+                    self.category.append(topic)
+        return self.category
+
+
+if __name__ == '__main__':
+    post = Post('2019.11.12', '급식 먹기 싫어요 폰도 안 내면 안 돼요?')
+    print(post.find_category())
