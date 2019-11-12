d6b8ba327d7003fd9416dbc4924f1fe64ab8ff1c
hyun020215 <hyun020215@sasa.hs.kr>
2019-11-12 23:13:03 +0900

    Post class created Graph class created README.md completed

--- graphs.py
+++ graphs.py
@@ -1,9 +1,13 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 
 class Graph:
     @ staticmethod
     def bar_graph(title, x, y, x_label, y_label):
+        font_name = mpl.font_manager.FontProperties(fname='C:/Windows/Fonts/malgun.ttf').get_name()
+        mpl.rc('font', family=font_name)
+
         plt.bar(x, y, width=0.3)
         plt.xlabel(x_label)
         plt.ylabel(y_label)
@@ -12,6 +16,9 @@ class Graph:
 
     @ staticmethod
     def line_graph(title, x, y, x_label, y_label):
+        font_name = mpl.font_manager.FontProperties(fname='C:/Windows/Fonts/malgun.ttf').get_name()
+        mpl.rc('font', family=font_name)
+
         plt.plot(x, y)
         plt.xlabel(x_label)
         plt.ylabel(y_label)
@@ -20,4 +27,4 @@ class Graph:
 
 
 if __name__ == '__main__':
-    Graph.bar_graph(title='practice', x=['dorm', 'cafe', 'suggest'], y=[3, 5, 2], x_label='topic', y_label='number')
+    Graph.bar_graph(title='주제별 게시물 건수', x=['기숙사', '급식', '건의사항'], y=[3, 5, 2], x_label='주제', y_label='건수')
