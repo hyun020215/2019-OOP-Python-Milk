56d26ee74f5190a88281a197fe83074a9b8322b5
hyun020215 <55475492+hyun020215@users.noreply.github.com>
2019-11-13 11:50:35 +0900

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -4,27 +4,34 @@ import matplotlib.pyplot as plt
 
 class Graph:
     @ staticmethod
-    def bar_graph(title: str, x: list, y: list, x_label: str, y_label: str) -> None:
+    def bar_graph(title: str, x: list, y: dict, x_label: str, y_label: str) -> None:
         font_name = mpl.font_manager.FontProperties(fname='C:/Windows/Fonts/malgun.ttf').get_name()
         mpl.rc('font', family=font_name)
 
-        plt.bar(x, y, width=0.3)
+        values = list(y.items())
+        for i in range(len(values)):
+            label, value = values[i]
+            plt.bar([j+0.1*(-len(values)+1+2*i) for j in range(len(x))], value, width=0.2, label=label)
+        plt.xticks([i for i in range(len(x))], x)
         plt.xlabel(x_label)
         plt.ylabel(y_label)
+        plt.legend()
         plt.title(title)
         plt.show()
 
     @ staticmethod
-    def line_graph(title: str, x: list, y: list, x_label: str, y_label: str) -> None:
+    def line_graph(title: str, x: list, y: dict, x_label: str, y_label: str) -> None:
         font_name = mpl.font_manager.FontProperties(fname='C:/Windows/Fonts/malgun.ttf').get_name()
         mpl.rc('font', family=font_name)
 
-        plt.plot(x, y)
+        for label, value in y.items():
+            plt.plot(x, value, label=label, marker='o')
         plt.xlabel(x_label)
         plt.ylabel(y_label)
+        plt.legend()
         plt.title(title)
         plt.show()
 
 
 if __name__ == '__main__':
-    Graph.bar_graph(title='주제별 게시물 건수', x=['기숙사', '급식', '건의사항'], y=[3, 5, 2], x_label='주제', y_label='건수')
+    Graph.bar_graph(title='주제별 게시물 건수', x=['기숙사', '급식', '건의사항'], y={'post': [3, 5, 2], 'what': [1, 2, 3], 'who': [3,2,1]}, x_label='주제', y_label='건수')
