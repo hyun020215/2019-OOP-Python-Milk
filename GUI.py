dc0af96981d325171fcef223c0686150cee16d14
hyun020215 <hyun020215@sasa.hs.kr>
2019-11-13 22:51:24 +0900

    GUI experiment (diverse way)

--- GUI.py
+++ GUI.py
@@ -1,8 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QPushButton, QToolTip, QMainWindow, QAction, qApp, QWidget, QDesktopWidget
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import QCoreApplication
 
 
-class MyApp(QWidget):
+class MyApp(QMainWindow, QWidget):
 
     def __init__(self):
         super().__init__()
@@ -10,11 +12,43 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
+        exitAction = QAction(QIcon('exit.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+
+        self.statusBar()
+
+        self.toolbar = self.addToolBar('Exit')
+        self.toolbar.addAction(exitAction)
+
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        self.statusBar().showMessage('Ready')
+
+        QToolTip.setFont(QFont('SansSerif', 10))
+        self.setToolTip('This is a <b>QWidget</b> widget')
+
+        btn = QPushButton('Quit', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.move(300, 150)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(QCoreApplication.instance().quit)
 
         self.setWindowTitle('My First Application')
-        self.move(300, 300)
+        self.setWindowIcon(QIcon('icon.png'))
         self.resize(400, 200)
+        self.center()
         self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 
 if __name__ == '__main__':
