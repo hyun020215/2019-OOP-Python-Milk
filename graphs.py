c96855cbb6643dc767bb43102c89747630045316
hyun020215 <hyun020215@sasa.hs.kr>
2019-11-14 14:31:52 +0900

    Pie graph added

--- graphs.py
+++ graphs.py
@@ -26,7 +26,7 @@ class Graph:
         plt.legend()
         plt.title(title)
 
-        # 그래프 시각화
+        # 그래프 띄우기
         plt.show()
 
     # 꺾은선그래프
@@ -47,9 +47,26 @@ class Graph:
         plt.legend()
         plt.title(title)
 
-        # 그래프 시각화
+        # 그래프 띄우기
+        plt.show()
+
+    # 원그래프
+    # 매개변수 - title: 그래프 제목, data: 데이터 셋 {범주: 값} 묶음
+    @ staticmethod
+    def pie_graph(title: str, data: dict):
+        # 한글 폰트 설정: 나눔스퀘어레귤러
+        font_name = mpl.font_manager.FontProperties(fname='C:/Windows/Fonts/NanumSquareR.ttf').get_name()
+        mpl.rc('font', family=font_name)
+
+        # 그래프에 값 표시
+        plt.pie(list(data.values()), labels=list(data.keys()), startangle=90, shadow=True, autopct='%1.1f%%')
+
+        # 그래프 제목 표시
+        plt.title(title)
+
+        # 그래프 띄우기
         plt.show()
 
 
 if __name__ == '__main__':
-    Graph.bar_graph(title='주제별 게시물 건수', x=['기숙사', '급식', '건의사항'], y={'post': [3, 5, 2], 'what': [1, 2, 3], 'who': [3, 2, 1]}, x_label='주제', y_label='건수')
+    Graph.pie_graph(title='국가별 ??? 기여도', data={'Korea': 124, 'Japan': 235, 'America': 412})
