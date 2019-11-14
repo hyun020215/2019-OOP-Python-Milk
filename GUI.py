6c3dfd2624bd9b81879cda167e63b58f7421224b
hyun020215 <hyun020215@sasa.hs.kr>
2019-11-14 23:35:20 +0900

    elementary layout settings

--- GUI.py
+++ GUI.py
@@ -1,3 +1,4 @@
+"""
 import sys
 from PyQt5.QtWidgets import QApplication, QPushButton, QToolTip, QMainWindow, QAction, qApp, QWidget, QDesktopWidget
 from PyQt5.QtGui import QIcon, QFont
@@ -56,3 +57,103 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyApp()
     sys.exit(app.exec_())
+"""
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_up_UI()
+
+    def set_up_UI(self):
+        period_start = QDateEdit(self)
+        period_end = QDateEdit(self)
+
+        period = QHBoxLayout()
+        period.addStretch(1)
+        period.addWidget(period_start)
+        period.addWidget(QLabel('~', self))
+        period.addWidget(period_end)
+        period.addStretch(1)
+
+        search_button = QPushButton('검색', self)
+
+        set_condition = QVBoxLayout()
+        set_condition.addLayout(period)
+        set_condition.addWidget(search_button)
+        set_condition.addStretch(8)
+
+        result_list = QListView(self)
+
+        search_result = QVBoxLayout()
+        search_result.addWidget(QLabel('검색 결과', self))
+        search_result.addWidget(result_list)
+
+        search = QHBoxLayout()
+        search.addStretch(1)
+        search.addLayout(search_result)
+        search.addStretch(1)
+        search.addLayout(set_condition)
+        search.addStretch(1)
+
+        self.resize(1200, 600)
+        self.center()
+        self.setWindowTitle("SASA Bamboo Analyzer")
+        self.setWindowIcon(QIcon('images/icon.png'))
+
+        # self.lineEdit = QLineEdit()
+        # self.pushButton = QPushButton("차트그리기", self)
+        # self.pushButton.clicked.connect(self.push_button_clicked)
+
+        self.fig = plt.Figure()
+        self.canvas = FigureCanvas(self.fig)
+
+        # left_layout = QVBoxLayout()
+        # left_layout.addWidget(self.canvas)
+        #
+        # # Right Layout
+        # right_layout = QVBoxLayout()
+        # right_layout.addWidget(self.lineEdit)
+        # right_layout.addWidget(self.pushButton)
+        # right_layout.addStretch(1)
+        #
+        # layout = QHBoxLayout()
+        # layout.addLayout(left_layout)
+        # layout.addLayout(right_layout)
+        # layout.setStretchFactor(left_layout, 1)
+        # layout.setStretchFactor(right_layout, 0)
+
+        # self.setLayout(layout)
+
+        self.setLayout(search)
+
+    # def push_button_clicked(self):
+    #     print(self.lineEdit.text())
+
+    def center(self):
+        position = self.frameGeometry()
+        center_point = QDesktopWidget().availableGeometry().center()
+        position.moveCenter(center_point)
+        self.move(position.topLeft())
+
+    def closeEvent(self, event):
+
+        reply = QMessageBox.question(self, 'Message', 'Are you sure to quit?',
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    app.exec_()
